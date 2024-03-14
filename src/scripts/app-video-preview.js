a2f20622e778c91c42c348d8a8fd64714a8c98f7
alexnozer <lescha.nazarenko@gmail.com>
2024-03-14 23:55:02 +0300

    Fix video component

--- src/scripts/app-video-preview.js
+++ src/scripts/app-video-preview.js
@@ -1,68 +1,68 @@
-// class AppVideoPreview extends HTMLElement {
-// 	static parentDelegateElements = 'article.card';
-// 	picture = null;
-// 	video = null;
-// 	target = null;
-// 	playPromise = null;
+class AppVideoPreview extends HTMLElement {
+	static parentDelegateElements = 'article.card';
+	picture = null;
+	video = null;
+	target = null;
+	playPromise = null;
 
-// 	connectedCallback() {
-// 		this.picture = this.querySelector('picture'); 
-// 		this.video = this.querySelector('video');
+	connectedCallback() {
+		this.picture = this.querySelector('picture'); 
+		this.video = this.querySelector('video');
 
-// 		if (!this.picture && !this.video) {
-// 			console.warn('app-video-preview should contains <picture> and <video> elements.');
-// 			return;
-// 		}
+		if (!this.picture && !this.video) {
+			console.warn('app-video-preview should contains <picture> and <video> elements.');
+			return;
+		}
 
-// 		const parent = this.parentElement;
-// 		this.target = this;
+		const parent = this.parentElement;
+		this.target = this;
 
-// 		if (parent.matches(AppVideoPreview.parentDelegateElements)) {
-// 			this.target = parent;
-// 		}
+		if (parent.matches(AppVideoPreview.parentDelegateElements)) {
+			this.target = parent;
+		}
 
-// 		if (!this.video.autoplay) {
-// 			this.target.addEventListener('pointerenter', this.playVideo.bind(this));
-// 			this.target.addEventListener('pointerleave', this.stopVideo.bind(this));
-// 		} else {
-// 			this.target.addEventListener('click', this.toggleVideo.bind(this));
-// 		}
-// 	}
+		if (!this.video.autoplay && this.video.src) {
+			this.target.addEventListener('pointerenter', this.playVideo.bind(this));
+			this.target.addEventListener('pointerleave', this.stopVideo.bind(this));
+		} else {
+			this.target.addEventListener('click', this.toggleVideo.bind(this));
+		}
+	}
 
-// 	disconnectedCallback() {
-// 		if (!this.video.autoplay) {
-// 			this.target.removeEventListener('pointerenter', this.playVideo.bind(this));
-// 			this.target.removeEventListener('pointerleave', this.stopVideo.bind(this));
-// 		} else {
-// 			this.target.removeEventListener('click', this.toggleVideo.bind(this));
-// 		}
-// 	}
+	disconnectedCallback() {
+		if (!this.video.autoplay) {
+			this.target.removeEventListener('pointerenter', this.playVideo.bind(this));
+			this.target.removeEventListener('pointerleave', this.stopVideo.bind(this));
+		} else {
+			this.target.removeEventListener('click', this.toggleVideo.bind(this));
+		}
+	}
 
-// 	playVideo() {
-// 		this.picture.hidden = true;
-// 		this.video.hidden = false;
-// 		this.playPromise = this.video.play();
-// 	}
+	playVideo() {
+		if (this.picture) this.picture.hidden = true;
+		this.video.hidden = false;
+		this.playPromise = this.video.play();
+	}
 
-// 	stopVideo() {
-// 		if (this.playPromise) {
-// 			this.playPromise.then(_ => {
-// 				this.video.pause();
-// 			});
-// 		}
+	stopVideo() {
+		if (this.playPromise) {
+			this.playPromise.then(_ => {
+				this.video.pause();
+			});
+		}
 
-// 		this.video.currentTime = 0;
-// 		this.picture.hidden = false;
-// 		this.video.hidden = true;
-// 	}
+		this.video.currentTime = 0;
+		if (this.picture) this.picture.hidden = false;
+		this.video.hidden = true;
+	}
 
-// 	toggleVideo() {
-// 		if (this.video.paused) {
-// 			this.video.play();
-// 		} else {
-// 			this.video.pause();
-// 		}
-// 	}
-// }
+	toggleVideo() {
+		if (this.video.paused) {
+			this.video.play();
+		} else {
+			this.video.pause();
+		}
+	}
+}
 
-// customElements.define('app-video-preview', AppVideoPreview);
+customElements.define('app-video-preview', AppVideoPreview);
